9b1c5a73715e2f37f543b0cd8273af1f6f2e1be9
Carlos Leonardo Machado Lago <carlos.leonardo@dinfor.uniceub>
2025-06-02 21:27:36 -0300

    added: account lock

--- Authentication/Username enumeration via account lock/main.py
+++ Authentication/Username enumeration via account lock/main.py
@@ -9,3 +9,17 @@
 
   Author: D4mn20
 """
+import argparse
+import time
+
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+requests.packages.disable_warnings(InsecureRequestWarning)
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-u", "--url", required=True, help="Target URL")
+  parser.add_argument("--proxy")
+
+if __init__ == "__main___":
+  main()
